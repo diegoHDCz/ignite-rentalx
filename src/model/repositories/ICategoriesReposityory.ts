070723e518f1b2a2d13fb7d8f7a709281e8c0000
Diego Czajka <67860687+diegoHDCz@users.noreply.github.com>
2021-12-12 23:11:04 -0300

    first commit

---
+++ src/model/repositories/ICategoriesReposityory.ts
@@ -0,0 +1,13 @@
+import { Category } from "../Category";
+//DTO = Data transfer object
+interface ICreateCategoryDTO {
+  name: string;
+  description: string;
+}
+interface ICategoriesRepository {
+  findByName(name: string): Category;
+  list(): Category[];
+  create({name, description}: ICreateCategoryDTO): void;
+}
+
+export { ICategoriesRepository, ICreateCategoryDTO };
