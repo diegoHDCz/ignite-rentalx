98819eeeff5fb2cfd45faf6b3d3257dda6bd78cf
Diego Czajka <67860687+diegoHDCz@users.noreply.github.com>
2021-12-28 23:21:51 -0300

    test list cars and admin-middleware

--- src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
+++ src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
@@ -15,7 +15,7 @@ class CarsRepositoryInMemory implements ICarsRepository {
   }: ICreateCarsDTO): Promise<Car> {
     const car = new Car();
 
-    Object.assign(car,{
+    Object.assign(car, {
       name,
       description,
       brand,
@@ -32,6 +32,26 @@ class CarsRepositoryInMemory implements ICarsRepository {
 
   async findByLicensePlate(license_plate: string): Promise<Car> {
     return this.cars.find((car) => car.license_plate === license_plate);
+  }
+
+  async findAvailable(
+    brand?: string,
+    category_id?: string,
+    name?: string
+  ): Promise<Car[]> {
+    const all = this.cars.filter((car) => {
+      if (
+        car.available === true ||
+        ((brand && car.brand === brand) ||
+          (category_id && car.category_id === category_id) ||
+          (name && car.name === name))
+      ) {
+        return car;
+      }
+      return null;
+    });
+
+    return all;
   }
 }
 
